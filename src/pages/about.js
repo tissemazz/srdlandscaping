1790658c368d76acd2a0b8c65f4af6a7dee75e73
tissemazz <tissemazz@gmail.com>
2022-05-18 17:11:50 +1000

    finished tutorial 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return(
+        <main>
+            <Layout pageTitle="About us at SRD">
+            <h2>G'day and welcome to SRD Landscaping.</h2>
+            <p>We are an all rounder, jack of all trades is better than a master of one, Landscape and Construction company</p>
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage
